548fa01bbdcdfa584a66fdc63007506d92e249ae
Cyusa <cyusagedeon@gmail.com>
2021-05-02 15:10:33 +0200

    Heroku config

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '#0=*iyhkwe1xu!1rzel)g2l(@-_6i66=n!ao3j^j1s%5l@iu@x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
